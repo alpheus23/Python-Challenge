f91c37809e621ceca5b1675343d6bb8f60936cf7
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-09-19 17:22:27 -0400

    Working through taking the average change, running into errors.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,9 @@ import csv
 
 #Declare variables
 monthTotal = 0
+profitLosses = 0
+changes = []
+averageChange = 0.0
 
 #Create os path to reach csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
@@ -18,5 +21,13 @@ with open(csvpath) as csvfile:
     #Read through each row of data after header
     for row in csvReader:
         monthTotal += 1
+        profitLosses += int(row[1])
+            
+        change = int(row[1]) - next(int(row[1]))
+        changes.append(change)
+        print(changes)
 
+
+    #Print out the Financial Analysis
     print(f"Total Months: {monthTotal}")
+    print(f"Total: ${profitLosses}")
