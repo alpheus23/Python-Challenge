e2b258b8386ff4b045b93315fc16742e657035a2
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-09-20 13:19:45 -0400

    Removed placeholder file and calculated percentages of each candidate vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,6 +28,7 @@ with open(csvpath) as csvFile:
     #Skip over the first row of the csv file
     csvHeader = next(csvReader)
 
+    #Loop through each row and add counters to candidate votes based on values in row[2]
     for row in csvReader:
         totalVotes += 1
 
@@ -40,13 +41,21 @@ with open(csvpath) as csvFile:
         elif row[2] == "Li":
             liVotes += 1
 
-        else:
+        elif row[2] == "O'Tooley":
             tooleyVotes += 1
-    
 
+#Calculate percentage of votes each candidate received
+khanPercent = (khanVotes / totalVotes) * 100
+correyPercent = (correyVotes / totalVotes) * 100
+liPercent = (liVotes / totalVotes) * 100
+tooleyPercent = (tooleyVotes / totalVotes) * 100
 
 #Print out Election Results
 print("Election Results")
 print("---------------------------")
 print(f"Total Votes: {totalVotes}")
 print("---------------------------")
+print(f"Khan: {khanPercent:.2f}% ({khanVotes})")
+print(f"Correy: {correyPercent:.2f}% ({correyVotes})")
+print(f"Li: {liPercent:.2f}% ({liVotes})")
+print(f"O'Tooley: {tooleyPercent:.2f}% ({tooleyVotes})")
