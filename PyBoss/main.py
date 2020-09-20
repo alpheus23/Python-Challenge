0d2e562df5730f7db8cab52c1e7b1600becc4626
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-09-20 17:47:01 -0400

    Figured out how to separate first and last name and put them into their respective lists

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -4,13 +4,15 @@ import csv
 
 employeeCSV = os.path.join("PyBoss", "employee_data.csv")
 
-#Lists to store data
+#Declare variables
 empID = []
 firstName = []
 lastName = []
 DOB = []
 SSN = []
 State = []
+first = ""
+last = ""
 
 #Open CSV file
 with open(employeeCSV) as csvFile:
@@ -21,6 +23,18 @@ with open(employeeCSV) as csvFile:
 
     #Read through each row and assign appropriate data to appropriate list
     for row in csvReader:
+        #Add Employee ID
         empID.append(row[0])
+
+        #Split name value into first and last
+        first = row[1]
+        firstName.append(first.split())
+
+        #Assign second value in firstName[] to secondName[]
+        last = firstName.pop()
+        firstName.append(last[0])
+        lastName.append(last[1])
     
-print(f"{empID}")
+#print(f"{empID}")
+print(f"{firstName}")
+print(f"{lastName}")
