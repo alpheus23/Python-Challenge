d07d383e203c11c7c0ead8e9195a9a7fe4f2cd04
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-09-20 13:48:38 -0400

    Created txt file with Election Results printed in it.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -81,3 +81,23 @@ print(f"O'Tooley: {tooleyPercent:.2f}% ({tooleyVotes})")
 print("---------------------------")
 print(f"Winner: {winner}")
 print("---------------------------")
+
+#Specify the file to write to  
+outputPath = os.path.join("Analysis", "analysis.txt")
+
+#Open the file using "write" mode. Specify the variable to hold the contents
+with open(outputPath, 'w') as txtFile:
+
+    #Recreate Election Results as a single print statement to be pushed to txt file
+    outputTxt = ("Election Results\n"
+                "---------------------------\n"
+                f"Total Votes: {totalVotes}\n"
+                "---------------------------\n"
+                f"Khan: {khanPercent:.2f}% ({khanVotes})\n"
+                f"Correy: {correyPercent:.2f}% ({correyVotes})\n"
+                f"Li: {liPercent:.2f}% ({liVotes})\n"
+                f"O'Tooley: {tooleyPercent:.2f}% ({tooleyVotes})\n"
+                "---------------------------\n"
+                f"Winner: {winner}\n"
+                "---------------------------")
+    txtFile.write(outputTxt)
